2d090f692066627c5cf9e0cf6b74a2e42991c42b
chelluboinajyothiswarup <100918884+chelluboinajyothiswarup@users.noreply.github.com>
2023-06-19 17:56:40 +0530

    Create 1. Two Sum.py

---
+++ 1. Two Sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n=len(nums)
+        l=[]
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i]+nums[j]==target:
+                    l.append(i)
+                    l.append(j)
+                    break
+            if len(l)==2:
+                break
+        return l
